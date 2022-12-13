3885577355535e82adbe59df0b982cf3ce76d3d5
dennisnderitu254 <dknderitu@gmail.com>
2022-12-13 17:11:47 +0300

    advanced tasks

--- test.c
+++ test.c
@@ -5,5 +5,7 @@
 int printf(const char *format, ...)
 {
     // This will be used to print text
-    write(1, "Congratulations, you win the Jackpot!", 37);
+    write(1, "9 8 10 24 75 - 9\n", 17);
+    write(1, "Congratulations, you win the Jackpot!\n", 38);
+    exit(EXIT_SUCCESS);
 }
